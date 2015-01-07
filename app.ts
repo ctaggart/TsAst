649fc9ada0e75940276ba99ab4f9e324e46c8495
Cameron Taggart <cameron.taggart@gmail.com>
2015-01-07 12:29:23 -0800

    started trying the create nodes and emit them

--- app.ts
+++ app.ts
@@ -1,38 +1,17 @@
 ﻿///<reference path='../TypeScript/built/local/typescript.d.ts' />
-///<reference path='node.d.ts' />
 
 import ts = require('typescript');
-import fs = require('fs');
-import path = require('path');
-
-// derived from getAllBinaryExpressions https://github.com/Microsoft/TypeScript/issues/254
-function getAllInterfaces(root: ts.Node) {
-    var result: ts.InterfaceDeclaration[] = [];
-    aggregate(root);
-    return result;
-
-    function aggregate(node: ts.Node): void {
-        console.log('node: ' + (ts.tokenToString(node.kind)));
-        if (node.kind === ts.SyntaxKind.InterfaceDeclaration) {
-            result.push(<ts.InterfaceDeclaration>node);
-        }
-        ts.forEachChild(node, aggregate);
-    }
-}
 
 export function main() {
-    var filename = process.cwd() + '/../TypeScript/built/local/typescript.d.ts'
-    var source = String(fs.readFileSync(filename));
-    //console.log("source: " + source);
+    var m = <ts.ModuleDeclaration>ts.createNode(ts.SyntaxKind.ModuleDeclaration);
+    var nm = <ts.Identifier>ts.createNode(ts.SyntaxKind.Identifier);
+    nm.text = "Blah"
+    m.name = nm;
+    
+    var sf = <ts.SourceFile>ts.createNode(ts.SyntaxKind.SourceFile);
+    sf.statements = <ts.NodeArray<ts.ModuleElement>>[m];
 
-    var scanner = ts.createScanner(ScriptTarget.Latest, true, source);
-    var root = ts.createNode(scanner.scan());
-
-    getAllInterfaces(root).forEach(ifd => {
-        console.log('interface: ' + ifd.name);
-    });
-
-    console.log('done');
+    console.log("file: "+sf.text); // sf.text is undefined :(
 }
 
 main();
