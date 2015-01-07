eb416d36727f7d79c1661823fb4022416c13941e
Cameron Taggart <cameron.taggart@gmail.com>
2015-01-06 21:24:55 -0800

    started switching to TypeScript 1.4

--- app.ts
+++ app.ts
@@ -1,53 +1,38 @@
-﻿///<reference path='typescriptServices.d.ts' />
-
-// causes IntelliSense to partially work
-// needed by Node.js at runtime, but comment out to use IntelliSense
-var TypeScript = require('./typescriptServices');
-
-// node.d.ts redefines require(), so must be below above statement
+﻿///<reference path='../TypeScript/built/local/typescript.d.ts' />
 ///<reference path='node.d.ts' />
+
+import ts = require('typescript');
 import fs = require('fs');
 import path = require('path');
 
-export function getName(ast: TypeScript.AST) {
-    var k = ast.kind();
-    switch (k) {
-        case TypeScript.SyntaxKind.IdentifierName:
-            return (<TypeScript.Identifier>ast).text();
-        case TypeScript.SyntaxKind.QualifiedName:
-            var qn = <TypeScript.QualifiedName>ast;
-            return getName(qn.left) + '.' + getName(qn.right);
-        default:
-            return TypeScript.SyntaxKind[k];
+// derived from getAllBinaryExpressions https://github.com/Microsoft/TypeScript/issues/254
+function getAllInterfaces(root: ts.Node) {
+    var result: ts.InterfaceDeclaration[] = [];
+    aggregate(root);
+    return result;
+
+    function aggregate(node: ts.Node): void {
+        console.log('node: ' + (ts.tokenToString(node.kind)));
+        if (node.kind === ts.SyntaxKind.InterfaceDeclaration) {
+            result.push(<ts.InterfaceDeclaration>node);
+        }
+        ts.forEachChild(node, aggregate);
     }
 }
 
 export function main() {
-    var filename = process.cwd() + '/typescriptServices.d.ts'
+    var filename = process.cwd() + '/../TypeScript/built/local/typescript.d.ts'
     var source = String(fs.readFileSync(filename));
-    var syntaxTree = TypeScript.Parser.parse('typescriptServices.d.ts', TypeScript.SimpleText.fromString(source), /*isDeclaration*/ true ,
-        new TypeScript.ParseOptions(TypeScript.LanguageVersion.EcmaScript5, /*autoSemicolon*/ true));
+    //console.log("source: " + source);
 
-    var cs = new TypeScript.CompilationSettings();
-    cs.codeGenTarget = TypeScript.LanguageVersion.EcmaScript5;
-    var ics = TypeScript.ImmutableCompilationSettings.fromCompilationSettings(cs);
-    var sourceUnit = TypeScript.SyntaxTreeToAstVisitor.visit(syntaxTree, 'typescriptServices.d.ts', ics, /*incrementalAST*/ false);
+    var scanner = ts.createScanner(ScriptTarget.Latest, true, source);
+    var root = ts.createNode(scanner.scan());
 
-    for (var i = 0, n = sourceUnit.moduleElements.childCount(); i < n; i++) {
-        var me = sourceUnit.moduleElements.childAt(i);
-        switch (me.kind()) {
-            case TypeScript.SyntaxKind.ModuleDeclaration:
-                var md = <TypeScript.ModuleDeclaration> me;
-                console.log('module ' + getName(md.name));
-                break;
-            case TypeScript.SyntaxKind.ClassDeclaration:
-                var cd = <TypeScript.ClassDeclaration> me;
-                console.log('class ' + cd.identifier.text());
-                break;
-            case TypeScript.SyntaxKind.VariableStatement:
-                break;
-        }
-    }
+    getAllInterfaces(root).forEach(ifd => {
+        console.log('interface: ' + ifd.name);
+    });
+
+    console.log('done');
 }
 
 main();
