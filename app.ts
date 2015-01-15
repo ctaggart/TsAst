9c2dd713ee9f58d4dac3fd5bb4fff9ed9379908f
Cameron Taggart <cameron.taggart@gmail.com>
2015-01-14 17:28:38 -0800

    cleaned up format code, split up functions

--- app.ts
+++ app.ts
@@ -2,49 +2,27 @@
 ///<reference path='../TypeScript/built/local/typescript_internal.d.ts' />
 import ts = require('typescript');
 
-// https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
-
-interface SourceFile {
+interface SourceFileVersion {
     filename: string;
     version: number;
     text: string;
 }
 
-export function format() {
-    var files: SourceFile[] = [
-        { filename: "file1.ts", version: 0, text: "class   Triangle { edges() { return 3; } }" },
-    ];
+// from https://github.com/Microsoft/TypeScript/issues/1651#issuecomment-69877863
+function formatCode(orig: string, changes: ts.TextChange[]): string {
+    var result = orig;
+    for (var i = changes.length - 1; i >= 0; i--) {
+        var change = changes[i];
+        var head = result.slice(0, change.span.start);
+        var tail = result.slice(change.span.start + change.span.length)
+        result = head + change.newText + tail;
+    }
+    return result;
+}
 
-    var servicesHost: ts.LanguageServiceHost = {
-        getScriptFileNames: () => files.map(f => f.filename),
-        getScriptVersion: (filename) => ts.forEach(files,
-            f => f.filename === filename ? f.version.toString() : undefined),
-        getScriptSnapshot: (filename) => {
-            var file = ts.forEach(files, f => f.filename === filename ? f : undefined);
-            var readText = () => file.text
-            return {
-                getText: (start, end) => readText().substring(start, end),
-                getLength: () => readText().length,
-                getLineStartPositions: () => [],
-                getChangeRange: oldSnapshot => undefined
-            };
-        },
-        log: message => console.log('log: ' + message),
-        getCurrentDirectory: () => '',
-        getScriptIsOpen: () => true,
-        getDefaultLibFilename: () => "lib.d.ts",
-        getLocalizedDiagnosticMessages: () => undefined,
-        getCancellationToken: () => undefined,
-        getCompilationSettings: () => { return {}; },
-        // missing from example
-        trace: message => console.log('trace: ' + message),
-        error: message => console.log('error: ' + message),
-    };
-
-    var services = ts.createLanguageService(servicesHost, ts.createDocumentRegistry())
-
-    // from src\harness\fourslash.ts
-    var formatCodeOptions: ts.FormatCodeOptions = {
+// 2015-01-13 from https://github.com/Microsoft/TypeScript/blob/master/src/harness/fourslash.ts#L350-L363
+function defaultFormatCodeOptions(): ts.FormatCodeOptions {
+    return {
         IndentSize: 4,
         TabSize: 4,
         NewLineCharacter: ts.sys.newLine,
@@ -58,22 +36,49 @@ export function format() {
         PlaceOpenBraceOnNewLineForFunctions: false,
         PlaceOpenBraceOnNewLineForControlBlocks: false,
     };
+}
 
-    var textChanges = services.getFormattingEditsForDocument("file1.ts", formatCodeOptions);
-    console.log('text changes: ' + textChanges.length);
-    textChanges.forEach(tc =>
-        console.log('tc: ' + tc.span.start + ' ' + tc.span.length + ' "' + tc.newText + '"'));
+// derived from https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API
+function inMemoryLanguageService(files: SourceFileVersion[]): ts.LanguageService {
+    var host: ts.LanguageServiceHost = {
+        getScriptFileNames: () => files.map(f => f.filename),
+        getScriptVersion: (filename) => ts.forEach(files,
+            f => f.filename === filename ? f.version.toString() : undefined),
+        getScriptSnapshot: (filename) => {
+            var file = ts.forEach(files, f => f.filename === filename ? f : undefined);
+            var readText = () => file.text
+            return {
+                getText: (start, end) => readText().substring(start, end),
+                getLength: () => readText().length,
+                getLineStartPositions: () => [],
+                getChangeRange: oldSnapshot => undefined
+            };
+        },
+        // TODO do something else with the log messages
+        log: message => undefined, //console.log('log: ' + message),
+        trace: message => undefined, //console.log('trace: ' + message),
+        error: message => undefined, //console.log('error: ' + message),
+        getCurrentDirectory: () => '',
+        getScriptIsOpen: () => true,
+        getDefaultLibFilename: () => "lib.d.ts",
+        getLocalizedDiagnosticMessages: () => undefined,
+        getCancellationToken: () => undefined,
+        getCompilationSettings: () => { return {}; },
+    };
+    return ts.createLanguageService(host, ts.createDocumentRegistry())
+}
 
-    var sf = services.getSourceFile("file1.ts");
-    textChanges.forEach(tc => {
-        var tcr: ts.TextChangeRange = {
-            span: tc.span,
-            newLength: tc.newText.length,
-        }
-        var b = sf.update(tc.newText, tcr);
-        console.log('b.text: ' + b.text);
-    });
-    console.log('sf.text: ' + sf.text);
+export function format() {
+    var f: SourceFileVersion = { filename: "triangle.ts", version: 0, 
+        text: "class   Triangle { edges() { return 3; }}" };
+    console.log('original: ' + f.text);
+    var langSvc = inMemoryLanguageService([f]);
+    var textChanges = langSvc.getFormattingEditsForDocument(f.filename, defaultFormatCodeOptions());
+    console.log('number of text changes: ' + textChanges.length);
+    textChanges.forEach(tc => console.log('start: ' + tc.span.start + ', length: '+ tc.span.length 
+        + ', new text: \'' + tc.newText + '\''));
+    var formatted = formatCode(f.text, textChanges);
+    console.log('formatted: ' + formatted);
 } 
 
 format();
