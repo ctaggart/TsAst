1ec3bc2721a92bc120855fd34906bc350b29aa51
Cameron Taggart <cameron.taggart@gmail.com>
2015-12-05 00:12:58 -0800

    stubbed out the EmitResolver and EmitHost for emitFiles

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
-﻿///<reference path='node_modules/typescript/lib/typescript.d.ts' />
+﻿/////<reference path='node_modules/typescript/lib/typescript.d.ts' />
 /////<reference path='../TypeScript/lib/typescript.d.ts' />
+///<reference path='../TypeScript/built/local/typescript.d.ts' />
 ///<reference path='node.d.ts' />
 
 import * as ts from "typescript";
@@ -9,6 +10,11 @@ import * as path from "path";
 // https://github.com/Microsoft/TypeScript/wiki/Using-the-Compiler-API#pretty-printer-using-the-ls-formatter
 // https://github.com/Microsoft/TypeScript/blob/master/src/compiler/parser.ts
 
+//class MyEmitResolver implements ts.EmitResolver {
+//    hasGlobalName(name: string) { return false; }
+
+//}
+
 export function main() {
     let source = "var a=function(v:number){return 0+1+2+3;}";
     let sourceFile = ts.createSourceFile("file.ts", source, ts.ScriptTarget.Latest, true);
@@ -17,27 +23,71 @@ export function main() {
     //let sourceFile = <ts.SourceFile>ts.createNode(ts.SyntaxKind.SourceFile);
     //sourceFile.text = "";
 
-    let stmt = <ts.VariableStatement>ts.createNode(ts.SyntaxKind.VariableStatement);
-    let vdl = <ts.VariableDeclarationList>ts.createNode(ts.SyntaxKind.VariableDeclarationList);
-    let vd = <ts.VariableDeclaration>ts.createNode(ts.SyntaxKind.VariableDeclaration);
-    let id = <ts.Identifier>ts.createNode(ts.SyntaxKind.Identifier);
-    let t = <ts.LiteralExpression>ts.createNode(ts.SyntaxKind.FirstLiteralToken);
-    t.text = "1";
-    id.text = "a";
-    vd.name = id;
-    vdl.declarations = <ts.NodeArray<ts.VariableDeclaration>>[vd];
-    stmt.declarationList = vdl;
+    //let stmt = <ts.VariableStatement>ts.createNode(ts.SyntaxKind.VariableStatement);
+    //let vdl = <ts.VariableDeclarationList>ts.createNode(ts.SyntaxKind.VariableDeclarationList);
+    //let vd = <ts.VariableDeclaration>ts.createNode(ts.SyntaxKind.VariableDeclaration);
+    //let id = <ts.Identifier>ts.createNode(ts.SyntaxKind.Identifier);
+    //let t = <ts.LiteralExpression>ts.createNode(ts.SyntaxKind.FirstLiteralToken);
+    //t.text = "1";
+    //id.text = "a";
+    //vd.name = id;
+    //vdl.declarations = <ts.NodeArray<ts.VariableDeclaration>>[vd];
+    //stmt.declarationList = vdl;
 
     //sourceFile.statements = <ts.NodeArray<ts.Statement>>[];
     //sourceFile.statements[0] = stmt;
-    sourceFile.statements = <ts.NodeArray<ts.Statement>>[<ts.Statement>stmt];
+    //sourceFile.statements = <ts.NodeArray<ts.Statement>>[<ts.Statement>stmt];
 
-    let formatCodeOptions = getDefaultOptions();
-    let rulesProvider = getRuleProvider(formatCodeOptions);
+    //let formatCodeOptions = getDefaultOptions();
+    //let rulesProvider = getRuleProvider(formatCodeOptions);
 
-    let edits = (<any>ts).formatting.formatDocument(sourceFile, rulesProvider, formatCodeOptions);
-    let code = applyEdits(sourceFile.text, edits);
-    console.log(code);
+    //let edits = (<any>ts).formatting.formatDocument(sourceFile, rulesProvider, formatCodeOptions);
+    //let code = applyEdits(sourceFile.text, edits);
+    //console.log(code);
+    
+    let resolver: ts.EmitResolver = {
+        hasGlobalName(name: string) { return false; },
+        getReferencedExportContainer(node: ts.Identifier) { return undefined; },
+        getReferencedImportDeclaration(node: ts.Identifier) { return undefined },
+        getReferencedNestedRedeclaration(node: ts.Identifier) { return undefined },
+        isNestedRedeclaration(node: ts.Declaration) { return false; },
+        isValueAliasDeclaration(node: ts.Node) { return false; },
+        isReferencedAliasDeclaration(node: ts.Node, checkChildren?: boolean) { return false; },
+        isTopLevelValueImportEqualsWithEntityName(node: ts.ImportEqualsDeclaration) { return false; },
+        getNodeCheckFlags(node: ts.Node) { return undefined; },
+        isDeclarationVisible(node: ts.Declaration) { return false; },
+        collectLinkedAliases(node: ts.Identifier) { return []; },
+        isImplementationOfOverload(node: ts.FunctionLikeDeclaration) { return false; },
+        writeTypeOfDeclaration(declaration: ts.AccessorDeclaration | ts.VariableLikeDeclaration, enclosingDeclaration: ts.Node, flags: ts.TypeFormatFlags, writer: ts.SymbolWriter) { },
+        writeReturnTypeOfSignatureDeclaration(signatureDeclaration: ts.SignatureDeclaration, enclosingDeclaration: ts.Node, flags: ts.TypeFormatFlags, writer: ts.SymbolWriter) { },
+        writeTypeOfExpression(expr: ts.Expression, enclosingDeclaration: ts.Node, flags: ts.TypeFormatFlags, writer: ts.SymbolWriter) { },
+        isSymbolAccessible(symbol: ts.Symbol, enclosingDeclaration: ts.Node, meaning: ts.SymbolFlags) { return undefined; },
+        isEntityNameVisible(entityName: ts.EntityName | ts.Expression, enclosingDeclaration: ts.Node) { return undefined; },
+        getConstantValue(node: ts.EnumMember | ts.PropertyAccessExpression | ts.ElementAccessExpression) { return 0; },
+        getReferencedValueDeclaration(reference: ts.Identifier) { return undefined; },
+        getTypeReferenceSerializationKind(typeName: ts.EntityName) { return undefined; },
+        isOptionalParameter(node: ts.ParameterDeclaration) { return false; },
+        isArgumentsLocalBinding(node: ts.Identifier) { return false; },
+        getExternalModuleFileFromDeclaration(declaration: ts.ImportEqualsDeclaration | ts.ImportDeclaration | ts.ExportDeclaration) { return undefined; }
+    }
+
+    let host: ts.EmitHost = {
+        getCompilerOptions() { return undefined; },
+        getSourceFile(fileName: string) { return undefined; },
+        getCurrentDirectory() { return undefined; },
+        getSourceFiles() { return []; },
+        getCommonSourceDirectory() { return undefined; },
+        getCanonicalFileName(fileName: string) { return undefined; },
+        getNewLine() { return undefined; },
+        isEmitBlocked(emitFileName: string) { return false; },
+        writeFile(fileName: string, data: string, writeByteOrderMark: boolean, onError?: (message: string) => void) {
+            console.log('fileName:' + fileName);
+            console.log('data:' + data);
+        }
+    }
+
+    let emitResult = ts.emitFiles(resolver, host, sourceFile);
+    console.log(emitResult);
 }
 
 function getRuleProvider(options: ts.FormatCodeOptions) {
