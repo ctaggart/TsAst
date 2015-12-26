a65c5651f9f1c053b61c89a8141e9d9e8bbfea67
Cameron Taggart <cameron@tachyus.com>
2015-12-26 10:28:52 -0800

    created F# project

---
+++ Edge.fs
@@ -0,0 +1,28 @@
+﻿[<AutoOpen>]
+module Taser.EdgeJs
+
+open System
+open System.Threading.Tasks
+open EdgeJs
+
+type Async with
+    static member Box (r:Async<'TResult>) =
+        async {
+            let! o = r
+            return box o
+        }
+
+    static member Unbox<'TResult> (r:Async<obj>) : Async<'TResult> =
+        async {
+            let! o = r
+            return unbox o
+        }
+
+type Edge with
+    static member Func f =
+        Func<obj,Task<obj>>(fun o -> f(unbox o) |> Async.Box |> Async.StartAsTask)
+
+    static member Async js =
+        fun arg ->
+            let func = Edge.Func js
+            func.Invoke arg |> Async.AwaitTask
---
+++ Program.fs
@@ -0,0 +1,22 @@
+﻿open EdgeJs
+open Taser
+
+let appendNetVersion (s:string) =
+    async { return sprintf "%s embedded in a .NET %s app" s (System.Environment.Version.ToString()) }
+
+type Funcs() =
+    member x.appendNetVersion = Edge.Func appendNetVersion
+
+let getVersions =
+    Edge.Async
+        """
+        return function(funcs, cb) {
+            funcs.appendNetVersion('Node.js ' + process.version, cb);
+        }
+        """
+
+[<EntryPoint>]
+let main argv =
+    let s = getVersions(Funcs()) |> Async.Unbox |> Async.RunSynchronously
+    printfn "This has %s written in F#." s
+    0
