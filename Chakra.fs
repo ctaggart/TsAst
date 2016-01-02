47e1a44c86c6bce96ae8c67012734d8f3ed55663
Cameron Taggart <cameron.taggart@gmail.com>
2016-01-01 22:22:41 -0800

    wrapped ts.createSourceFile

---
+++ Chakra.fs
@@ -0,0 +1,62 @@
+﻿[<AutoOpen>]
+module Taser.Chakra.ChakraExt
+
+open System
+open System.IO
+open System.Runtime.InteropServices
+open Taser
+open Taser.Chakra
+
+let readStreamToEnd stream =
+    use sr = new StreamReader(stream: Stream)
+    sr.ReadToEnd()
+
+let readFileToEnd file =
+    readStreamToEnd(File.OpenRead file)
+
+type ChakraContext() =
+    let sc = JavaScriptSourceContext.FromIntPtr IntPtr.Zero
+
+    let rt =
+        let ec, rt = Native.JsCreateRuntime(JavaScriptRuntimeAttributes.None, null)
+        Native.ThrowIfError ec
+        rt
+
+    let ctx =
+        let ec, ctx = Native.JsCreateContext rt
+        Native.ThrowIfError ec
+        let ec = Native.JsSetCurrentContext ctx
+        Native.ThrowIfError ec
+        ctx
+
+    member x.RunScript script = 
+        let ec, v = Native.JsRunScript(script, sc, String.Empty)
+        Native.ThrowIfError ec
+        v
+
+    member x.RunScriptFromStream stream =
+        x.RunScript (readStreamToEnd stream)
+
+    member x.RunScriptFromFile file =
+        x.RunScript (readFileToEnd file)
+
+    member x.CallFunction (fn, args) =
+        // the function is also the first argument
+        let args = Array.append [|fn|] args
+        let ec, v = Native.JsCallFunction(fn, args, uint16 args.Length)
+        Native.ThrowIfError ec
+        v
+
+type JavaScriptValue with
+    member x.GetPropertyByName name =
+        let id = JavaScriptPropertyId.FromString name
+        x.GetProperty id
+
+    member x.AsString = x.ToString()
+    member x.AsFloat = x.ToDouble()
+    member x.AsInt = int x.AsFloat
+
+    static member FromBooleanOption (v: bool option) =
+        match v with
+        | None -> JavaScriptValue.Undefined
+        | Some b -> JavaScriptValue.FromBoolean b
--- Program.fs
+++ Program.fs
@@ -1,8 +1,4 @@
-﻿open System
-open System.IO
-open System.Runtime.InteropServices
-open Taser
-open Taser.Chakra
+﻿open Taser.Chakra
 
 let printfn format = Printf.ksprintf System.Diagnostics.Debug.WriteLine format
 
@@ -13,174 +9,81 @@ type ScriptTarget =
     | ES2015 = 2
     | Latest = 2
 
-type SourceFile() =
-    member x.kind =
-        ()
-//        let fn = 
-//            Edge.Async
-//                """
-//                var ts = require('typescript');
-//                return function(p, cb) {
-//                    //cb(null, 'get kind: ' + p); // returns 'get kind: function (d, cb) { return f(d, cb, ctx); }'
-//                    //cb(null, p(null, true));
-//                    p(d, cb);
-//
-//                    // none of these seam to work // each returns 'get kind: undefined'
-//                    //cb(null, 'get kind: ' + p(function (x){ return x.kind; }) );
-//                    //cb(null, 'get kind: ' + p(function (x, xcb){ xcb( 'x: ' + x); }) );
-//                }
-//                """
-//        (fn obj) |> Async.Unbox
-//        async {
-//            let! o = fn args |> Async.Unbox
-//            return SourceFile o
-//        }
+type SourceFile(v: JavaScriptValue) = // TODO
+    member x.kind = (v.GetPropertyByName "kind").AsInt
 
+type TypeScriptServices(cc: ChakraContext) =
 
-//function createSourceFile(fileName: string, sourceText: string, languageVersion: ScriptTarget, setParentNodes?: boolean): SourceFile;
-type createSourceFileArgs = {
-    fileName: string
-    sourceText: string
-    languageVersion: ScriptTarget
-    setParentNodes: bool option
-}
+    let fcreateSourceFile  = cc.RunScript "ts.createSourceFile"
 
-//let createSourceFile (args: createSourceFileArgs) : Async<SourceFile> =
-//    let fn = 
-//        """
-//        var ts = require('typescript');
-//        return function(p, cb) {
-//            var o = ts.createSourceFile(p.fileName, p.sourceText, p.languageVersion, p.setParentNodes);
-//            //var o = 'args: ' + p.fileName + p.languageVersion + p.setParentNodes
-//            //cb(null, o);
-//            var sfcb = function (err, ca) {
-//                ca(o);
-//            };
-//            cb(null, sfcb);
-//        }
-//        """
-//    fn args |> Async.Unbox
-//    async {
-//        let! o = fn args |> Async.Unbox
-//        return SourceFile o
-//    }
+    member x.createSourceFile (fileName: string, sourceText: string, languageVersion: ScriptTarget, setParentNodes: bool option): SourceFile =
+        let args = 
+            [|  JavaScriptValue.FromString fileName
+                JavaScriptValue.FromString sourceText
+                JavaScriptValue.FromInt32 (int languageVersion)
+                JavaScriptValue.FromBooleanOption setParentNodes
+            |]
+        let v = cc.CallFunction (fcreateSourceFile, args)
+        SourceFile v
 
-let getValueType v =
-    
-    let ec, vt = Native.JsGetValueType v
-    Native.ThrowIfError ec
-    vt
+let printValueType (v:JavaScriptValue) =
+    printfn "ValueType: %A" v.ValueType
 
-let printValueType v =
-    printfn "ValueType: %A" (getValueType v)
-
-let getString v =
-    let ec, sr = Native.JsConvertValueToString v
-    Native.ThrowIfError ec
-    let ec, sp, _ = Native.JsStringToPointer sr
-    Native.ThrowIfError ec
-    Marshal.PtrToStringUni sp
-
-let passJsRefToFunction() =
-
-    // setup
-    let ec, rt = Native.JsCreateRuntime(JavaScriptRuntimeAttributes.None, null)
-    Native.ThrowIfError ec
-
-    let ec, ctx = Native.JsCreateContext rt
-    Native.ThrowIfError ec
-
-    let ec = Native.JsSetCurrentContext ctx
-    Native.ThrowIfError ec
-
-    let ec = Native.JsStartDebugging()
-    Native.ThrowIfError ec
+let passJsRefToFunction (cc: ChakraContext) =
 
     // Hello World
-    let sc = JavaScriptSourceContext.FromIntPtr IntPtr.Zero
-    let ec, v = Native.JsRunScript("'Hello' + ' World'", sc, String.Empty)
-    Native.ThrowIfError ec
-    printValueType v // String
-    printfn "string: %s" (getString v)
+    let hw = cc.RunScript "'Hello' + ' World'"
+    printfn "hw: %s" hw.AsString
 
     // create a function
-    let ec, createPerson = Native.JsRunScript("(function(){ return { name: 'Cameron', age: 36 } });", sc, String.Empty )
-    Native.ThrowIfError ec
-    printValueType v // Function
+    let createPerson = cc.RunScript "(function(){ return { name: 'Cameron', age: 36 } });"
+    printValueType createPerson // Function
 
     // call that function
-    let ec, person = Native.JsCallFunction(createPerson, [|createPerson|], uint16 1)
-    Native.ThrowIfError ec
+    let person = cc.CallFunction(createPerson, Array.empty)
     printValueType person // Object
 
     // get a property of the returned object
-    let pidname = JavaScriptPropertyId.FromString "name"
-    let ec, name = Native.JsGetProperty(person, pidname)
-    Native.ThrowIfError ec
-    printValueType name // String
-    printfn "name: %s" (getString name)
+    printfn "name: %s" ((person.GetPropertyByName "name").AsString)
 
     // get the number of properties of the object
-    let ec, names = Native.JsGetOwnPropertyNames person
-    Native.ThrowIfError ec
+    let names = person.GetOwnPropertyNames()
     printValueType names // Array
-    let pidlength = JavaScriptPropertyId.FromString "length"
-    let ec, plength = Native.JsGetProperty(names, pidlength)
-    Native.ThrowIfError ec
+    let plength = names.GetPropertyByName "length"
     printValueType plength // Number
     let ec, d = Native.JsNumberToDouble plength
-    printfn "length: %d" (int d)
+    printfn "length: %d" plength.AsInt
 
     // TypeScript Services
 
-    // load the js
-    use sr = new StreamReader(@"C:\ts\TsAst\node_modules\typescript\lib\typescriptServices.js")
-    let js = sr.ReadToEnd()
-    let ec, v = Native.JsRunScript(js, sc, String.Empty)
-    Native.ThrowIfError ec
-    printValueType v
-
     // get a function reference
-    let ec, createNode = Native.JsRunScript("ts.createNode", sc, String.Empty)
-    Native.ThrowIfError ec
+    let createNode = cc.RunScript "ts.createNode"
     printValueType createNode // Function
     printfn "createNode: "
 
     // calling the function
-    let ec, node = Native.JsCallFunction(createNode, [| createNode; JavaScriptValue.FromInt32 1 |], uint16 2)
-    Native.ThrowIfError ec
+    let node = cc.CallFunction(createNode, [| JavaScriptValue.FromInt32 1 |])
     printValueType node
     printfn "node: "
 
     // access a property on the returned object
-    let pidkind = JavaScriptPropertyId.FromString "kind"
-    let ec, pkind = Native.JsGetProperty(node, pidkind)
-    Native.ThrowIfError ec
-    printValueType pkind
-    printfn "  kind: %s" (getString pkind) // works even with it beign a number :)
+    printfn "  kind: %d" ((node.GetPropertyByName "kind").AsInt)
 
     // pass in the object to a custom function
-    let ec, getKind = Native.JsRunScript("(function(x){ return x.kind; })", sc, String.Empty)
-    Native.ThrowIfError ec
-    printValueType getKind // Function
-    let ec, kind = Native.JsCallFunction(getKind, [| getKind; node |], uint16 2)
-    Native.ThrowIfError ec
+    let getKind = cc.RunScript "(function(x){ return x.kind; })"
+    let kind = cc.CallFunction(getKind, [| node |])
     printValueType kind // Number
-    printfn "kind: %s" (getString kind)
-
-    ()
+    printfn "kind: %d" kind.AsInt
 
 [<EntryPoint>]
 let main argv =
+    let cc = ChakraContext()
+    let fn = @"C:\ts\TsAst\node_modules\typescript\lib\typescriptServices.js"
+    let js = readFileToEnd fn
+    cc.RunScript js |> ignore
+    let ts = TypeScriptServices cc
+    let sf = ts.createSourceFile(fn, js, ScriptTarget.Latest, None)
+    printfn "sf kind: %d" sf.kind 
 
-    // the TypeScript to translate
-
-    //    let fn = @"../../node_modules/typescript/lib/typescriptServices4.d.ts" |> Path.GetFullPath
-    //    printfn "loading %s" fn
-    //    let s = File.ReadAllText fn
-    //    let sf = createSourceFile { fileName = fn; sourceText = s; languageVersion = ScriptTarget.ES6; setParentNodes = None } |> Async.RunSynchronously
-    ////    printfn "sf %A" sf
-    //    printfn "sf kind: %A" (sf.kind |> Async.RunSynchronously)
-
-    passJsRefToFunction()
+//    passJsRefToFunction cc
     0
